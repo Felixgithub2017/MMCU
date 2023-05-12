fa92f87c40fcb64f63da5290ab1868059c330261
Hui ZENG <felix_zeng_ai@aliyun.com>
2023-05-12 16:04:55 +0800

    fixed some bugs

--- TestChatGPT.py
+++ TestChatGPT.py
@@ -5,15 +5,17 @@ import pandas as pd
 import time
 import requests
 import json
+import re
 
 choices = ["A", "B", "C", "D"]
 
-def softmax(x):
-    z = x - max(x)
-    numerator = np.exp(z)
-    denominator = np.sum(numerator)
-    softmax = numerator/denominator
-    return softmax
+def find_valid_substrings(s):
+    # 匹配长度为1到4的、不包含重复字符的子串
+    pattern = r'[ABCD]{1,4}'
+    substrings = re.findall(pattern, s)
+    # 过滤出不包含重复字符的子串
+    valid_substrings = [substring for substring in substrings if len(substring) == len(set(substring))]
+    return valid_substrings
 
 def format_subject(subject):
     l = subject.split("_")
@@ -28,7 +30,7 @@ def format_example(df, idx, include_answer=True):
     for j in range(k):
         #prompt += "\n{}. {}".format(choices[j], df.iloc[idx, j+1])
         try:
-            prompt += "\n{}. {}".format(choices[j], df.iloc[idx, j+1].replace("A、", "").replace("B、", "").replace("C、", "").replace("D、", "").replace("A.", "").replace("B.", "").replace("C.", "").replace("D.", "").replace("A", "").replace("B", "").replace("C", "").replace("D", "").replace("Ａ、", "").replace("Ｂ、", "").replace("Ｃ、", "").replace("Ｄ、", "").strip())
+            prompt += "\n{}. {}".format(choices[j], df.iloc[idx, j+1].replace(" ．", "").replace("A、", "").replace("B、", "").replace("C、", "").replace("D、", "").replace("A.", "").replace("B.", "").replace("C.", "").replace("D.", "").replace("A", "").replace("B", "").replace("C", "").replace("D", "").replace("Ａ、", "").replace("Ｂ、", "").replace("Ｃ、", "").replace("Ｄ、", "").strip())
         except Exception as e:
             prompt += "\n{}. {}".format(choices[j], df.iloc[idx, j+1])
     #prompt += "\nAnswer:"
@@ -79,7 +81,7 @@ def writetext_to_file(file, contents):
         f.write(contents)
 
 def eval(args, subject, dev_df, test_df):
-    logfile = "chatgptestlogfile"
+    logfile = "chatgptestlogfile0512"
     cors = []
     #labels = []
     preds = []
@@ -97,6 +99,8 @@ def eval(args, subject, dev_df, test_df):
 
         try:        
             label = test_df.iloc[i, test_df.shape[1]-1]
+            # A B C D 特殊处理
+            label = label.replace(" ", "").replace("Ａ", "A").replace("Ｂ", "B").replace("Ｃ", "C").replace("Ｄ", "D")
             print("正确答案:", label)
             with open(logfile, 'a', encoding='utf8') as f:
                 f.write("正确答案:"+label+"\n")
@@ -109,8 +113,9 @@ def eval(args, subject, dev_df, test_df):
                 time.sleep(1)
                 pred = plain_chat(prompt)
                 pred = pred.replace("、", "").replace(".", "").replace(",", "").replace(";", "").replace("，", "")
+                # 识别答案pattern
+                pred = find_valid_substrings(pred)[0]
                 print("模型预测答案:", pred)
-                preds.append(pred)
                 with open(logfile, 'a', encoding='utf8') as f:
                     f.write("模型预测答案:"+pred+"\n")
                 break
@@ -121,22 +126,16 @@ def eval(args, subject, dev_df, test_df):
                 continue
 
         try:
-            #cor = pred == label
-            if label in pred:
-                cor = True
-            else:
-                cor = False
+            cor = pred == label
             print("是否答对：", cor)
             with open(logfile, 'a', encoding='utf8') as f:
                 f.write("是否答对："+str(cor)+"\n")
             cors.append(cor)
+            preds.append(pred+"|||"+label+"|||"+str(cor))
 
             #labels.append(label)
         except Exception as e:
             print(e)
-
-        '''if i>=5:
-            break # test'''
 
     acc = np.mean(cors)
 
